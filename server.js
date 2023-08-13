57d08205cd4feefe71088baa5aa1641d866a541a
Harsh <hgaur491@gmail.com>
2023-08-13 16:47:22 +0530

    changing start script

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.json(parsed);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT}`);
 });
