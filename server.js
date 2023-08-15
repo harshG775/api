c64482ddca76754b307172187c49f9536c5bece0
Harsh <hgaur491@gmail.com>
2023-08-15 10:57:22 +0530

    adding html page

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+"use strict"
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const app = express();
 
 let db = fs.readFileSync("./database/db.json", "utf-8")
 let parsed = JSON.parse(db)
+
+app.use(express.static(path.join(__dirname,"public")))
+
 // main
 app.get("/", (req, res) => {
+    res.sendFile("./public/index.html")
+});
+app.get("/api/data", (req, res) => {
     res.json(parsed);
 });
 
